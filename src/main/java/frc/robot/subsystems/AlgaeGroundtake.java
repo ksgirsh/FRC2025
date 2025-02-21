c539c23bf7fcfcda34b82a496215722fed5a5a96
syomazh <108838306+syomazh@users.noreply.github.com>
2025-02-20 19:51:37 -0600

    utter bs

---
+++ src/main/java/frc/robot/subsystems/AlgaeGroundtake.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class AlgaeGroundtake extends SubsystemBase {
+  /** Creates a new AlgaeGroundtake. */
+  private static Elevator mInstance;
+
+  //wWHY DOES THIS CAUSE AN ERROR??
+  //PeriodicIO cannot be resolved to a type
+  //private PeriodicIO mPeriodicIO;
+
+
+
+  private AlgaeGroundtake() {
+    super("AlgaeGroundtake");
+  }
+
+  @Override
+  public void periodic() {
+
+  }
+}
